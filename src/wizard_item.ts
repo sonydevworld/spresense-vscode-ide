a4f09b24343972c0a6d922e3c70bf1086a9ffbca
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2019-10-21 10:54:50 +0900

    Add webview panel

--- src/wizard_item.ts
+++ src/wizard_item.ts
@@ -20,14 +20,85 @@
  */
 
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
+    const resourcePath = path.join(context.extensionPath, 'resources', 'wizard');
 
 	/* Register item wizard command */
 	context.subscriptions.push(vscode.commands.registerCommand('spresense.item.wizard', () => {
-
+        /* Open Workspace wizard */
+        ItemWizard.openWizard(resourcePath);
     }));
 }
 
 export function deactivate() {
+}
+
+class ItemWizard {
+
+    public static currentPanel: ItemWizard | undefined;
+
+    public static readonly viewType = 'ItemWizardView';
+
+    private readonly _panel: vscode.WebviewPanel;
+    private readonly _resourcePath: string | undefined;
+
+	private _disposables: vscode.Disposable[] = [];
+
+    public static openWizard(resourcePath: string) {
+        const column = vscode.window.activeTextEditor ? vscode.window.activeTextEditor.viewColumn : undefined;
+
+        if (ItemWizard.currentPanel) {
+            ItemWizard.currentPanel._panel.reveal(column);
+            return;
+        }
+
+        const panel = vscode.window.createWebviewPanel(
+            ItemWizard.viewType,
+            "Create new Item wizard",
+            column || vscode.ViewColumn.One,
+            {
+                enableScripts: true,
+                retainContextWhenHidden: true,
+                localResourceRoots: [
+                    vscode.Uri.file(resourcePath)
+                ]
+            });
+
+        /* Create new panel */
+        ItemWizard.currentPanel = new ItemWizard(panel, resourcePath);
+    }
+
+    private constructor(panel: vscode.WebviewPanel, resourcePath: string) {
+        this._resourcePath = resourcePath;
+        this._panel = panel;
+
+        this._panel.webview.onDidReceiveMessage(this.handleWebViewEvents, this, this._disposables);
+        this._panel.onDidDispose(() => this.dispose(), null, undefined);
+
+        this._panel.webview.html = this.getViewContent();
+    }
+
+    private dispose() {
+        ItemWizard.currentPanel = undefined;
+        this._panel.dispose();
+
+        while (this._disposables.length) {
+            const x = this._disposables.pop();
+            if (x) {
+                x.dispose();
+            }
+        }
+    }
+
+    private getViewContent(): string {
+        return "";
+    }
+
+    private handleWebViewEvents(message: any) {
+        if ('command' in message) {
+
+        }
+    }
 }
