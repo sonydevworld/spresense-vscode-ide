a96bd6e157b82da8bdfe95100c017051a6ebaafd
SPRESENSE <41312067+SPRESENSE@users.noreply.github.com>
2023-02-06 16:53:20 +0900

    scripts/flash_worker.sh: Remove esc character from script
    Esc character is no needed in shell script.

--- scripts/flash_worker.sh
+++ scripts/flash_worker.sh
@@ -4,7 +4,7 @@ SDKPATH=$1
 FPORT=$2
 FBAUD=$3
 
-if [ -n \"`ls out/worker/ 2>/dev/null`\" ]; then
+if [ -n "`ls out/worker/ 2>/dev/null`" ]; then
 	${SDKPATH}/sdk/tools/flash.sh -w \
 	                              -c ${FPORT} \
 	                              -b ${FBAUD} \
